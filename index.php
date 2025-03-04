62eadff87871fc92929446702da2a05ab6d7f3b7
Pratap Chandra kuldi <147026821+Pratapchandrakuldi@users.noreply.github.com>
2025-03-04 13:51:32 +0000

    index.php file uploaded

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <title>Admin Login</title>
+    <style>
+      #font {
+        width: 20px;
+        height: 20px;
+        margin: auto;
+        margin-top: 20px;
+        margin-left: 25%;
+      }
+    </style>
+
+  </head>
+
+  <body>
+    <header>
+      <div class="px-2 py-1 text-bg-dark">
+        <div class="container-fluid-lg my-2 gx-3">
+          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+            <li>
+              <a href="enduser/Ehome.php" class="nav-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="enduser/Aboutus.php" class="nav-link">
+                About US
+              </a>
+            </li>
+            <li>
+              <a href="enduser/Search_Doctor.php" class="nav-link">
+                Search Doctor
+              </a>
+            </li>
+            <li>
+              <a href="enduser/Contactus.php" class="nav-link">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a href="admin/adminlogin.php" class="nav-link">
+                Admin Log-in
+              </a>
+            </li>
+            <li>
+              <a href="doctor/Doctorlogin.php" class="nav-link">
+                Doctor Log-in
+              </a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </header>
+    <!-- start picture area -->
+    <div class="card text-bg-dark rounded-0 border border-0">
+      <img src="picture/homepic.png" height="400px" class="card-img rounded-0" alt="Image not found">
+    </div>
+    <!-- end picture area-->
+
+    <div style="margin:auto;">
+      <p style="font-size:30px;text-align:centre;padding-top:20px;" class="text-center"><b>Our Featured Service</b></p>
+    </div>
+
+    <div>
+      <p class="text-center" style="padding-left:40px;padding-right:40px;">
+        First thing to understand here is that websites like more are more than just healthcare professional & facility
+        directories .These websites also allow patients to
+        schedule appointments with doctors & clinics.Thus ,it becomes imprative that besides providing listing of
+        doctors, Our website also offers features
+        that help healthcare professionals to manage appointments & their medical practice seamlessly.
+      </p>
+    </div>
+
+
+
+
+    <!-- start footer area -->
+    <div class="container-fluid text-bg-dark" style="height:100px">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <span class="  align-items-left" style="margin-left:30px;margin-top:20px; ">
+          <p>&#169 Doctor appointment All right reserved.</p>
+        </span>
+        <span class="d-flex  align-items-center" id="font">
+          <i class="fa-brands fa-facebook-f fa-lg" style="color: #ffffff;"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+          <i class="fa-brands fa-twitter fa-lg" style="color: #ffffff;"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+          <b style="font-size:22px;">in</b>&nbsp;&nbsp;&nbsp;&nbsp;
+          <i class="fa-brands fa-pinterest fa-lg" style="color: #ffffff;"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+          <i class="fa-brands fa-google-plus-g fa-lg" style="color: #ffffff;"></i>
+        </span>
+      </footer>
+    </div>
+    <!-- end footer area -->
+
+
+    <!--javaScript link from bootsrap and jQuery link-->
+
+    <!-- <script src="..\js\bootstrap.js"></script> -->
+    <script src="C:\xampp\htdocs\project3\BookMyDoctor\fontawesome-free-6.4.0-web\js.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/ecf8f4889e.js" crossorigin="anonymous"></script>
+
+
+  </body>
+
+</html>
