7acf71cd985306061d9a8c1e97a8d6181c30bb66
Pratap Chandra kuldi <147026821+Pratapchandrakuldi@users.noreply.github.com>
2025-03-04 19:25:53 +0530

    Update connectiondatabase.php

--- connectiondatabase.php
+++ connectiondatabase.php
@@ -1,9 +1,9 @@
 <?php
      
-    $servername="localhost:3307";
-    $username="root";
-    $password="";
-    $dbname="bookmydoctor";
+    $servername="sql310.infinityfree.com";
+    $username="if0_38443966";
+    $password="vXdBk0uB1XPaa";
+    $dbname="if0_38443966_bookmydoctor";
     $conn=mysqli_connect($servername,$username,$password,$dbname);
     // if($conn){
     //     echo"connection is success";
